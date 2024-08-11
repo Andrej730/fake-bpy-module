3baa74cd8363d5884fe76cf5416231c8c6001e7d
nutti <nutti.metro@gmail.com>
2024-08-11 18:19:03 +0900

    WUIP

--- src/fake_bpy_module/transformer/mod_applier.py
+++ src/fake_bpy_module/transformer/mod_applier.py
@@ -75,6 +75,7 @@ class ModApplier(TransformerBase):
                         mod_options = mod_arg_node["mod-option"].split(",")
                     if "update-argument-type" in mod_options:
                         arg_node.attributes["argument_type"] = mod_arg_node.attributes["argument_type"]
+                    print(f"@@@@ {mod_arg_node.attributes}")
 
                     mod_desc_node = mod_arg_node.element(DescriptionNode)
                     if not mod_desc_node.empty():
